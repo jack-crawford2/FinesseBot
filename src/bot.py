82d638cba3a72d7f1b8ad01512b909424a663a3d
Jack Crawford <jack@Jacks-MBP-2.localdomain>
2020-12-18 17:53:22 -0600

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -85,7 +85,7 @@ class FinesseBot(BaseAgent):
         car_to_home_y = abs(-goaly - car_location.y)
         car_to_ball = car_location.dist(ball_location)
         car_to_nem = car_location.dist(nemesis_location)
-        if car_location.dist(ball_location) > 500 or (ball_location.x == 0 and ball_location.y == 0):
+        if ball_location.x == 0 and ball_location.y == 0:
             self.controller.boost = True
             self.aim(ball_location.x, ball_location.y, goaly)
         elif car_location.dist(nemesis_location) < 175:
