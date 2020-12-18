c57acb102684c40a5df914c6be630439841d68fa
Jack Crawford <jack@Jacks-MBP-2.localdomain>
2020-12-18 17:14:26 -0600

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -84,7 +84,7 @@ class FinesseBot(BaseAgent):
         ball_to_home_y = abs(-goaly - ball_location.y)
         car_to_home_y = abs(-goaly - car_location.y)
 
-        if car_location.dist(nemesis_location) < 300 and car_velocity < 200:
+        if car_location.dist(nemesis_location) < 300 and my_car.physics.velocity < 200:
             self.state = "flipStuck?"
             return self.begin_front_flip(packet)
         elif ball_to_home_y < car_to_home_y:
